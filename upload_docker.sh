113b0791760d05e87f6ed9c5d37f350f9129df78
Goodhope Ordu <goodhopeordu@yahoo.com>
2020-04-16 17:57:59 +0000

    task 4: upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=goodhopeordu/python-predictor
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=goodhopeordu
+docker tag python-predictor "$dockerpath":initial
 
 # Step 3:
 # Push image to a docker repository
+docker push "$dockerpath":initial
